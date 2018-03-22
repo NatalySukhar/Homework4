23839195c7e5c3d65c637d4da245c37671c44a86
NatalySukhar <natsuxar@gmail.com>
2018-03-22 03:08:01 +0200

    HW4

--- src/test/java/ua/DriverScript.java
+++ src/test/java/ua/DriverScript.java
@@ -3,10 +3,13 @@ package ua;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-
-import javax.security.auth.login.Configuration;
-import java.beans.EventHandler;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import java.io.File;
 
 import java.nio.file.Paths;
@@ -14,22 +17,41 @@ import java.util.concurrent.TimeUnit;
 
 
 public abstract class DriverScript {
-    public static WebDriver getDriver(){
-        System.setProperty("webdriver.gecko.driver", Paths.get(System.getProperty("user.dir"),"drivers","geckodriver.exe").toString());
-        WebDriver driver=new FirefoxDriver();
-        return driver;
-    }
-    public static EventFiringWebDriver getConfiguredDriver(){
-        WebDriver driver=getDriver();
-        driver.manage().window().maximize();
+    protected EventFiringWebDriver driver;
+    protected WebDriverWait wait;
+    public static WebDriver getDriver(String browser){
+        switch (browser){
+            case"firefox":
+                System.setProperty("webdriver.gecko.driver", Paths.get(System.getProperty("user.dir"),"drivers","geckodriver.exe").toString());
+                return new  FirefoxDriver();
+            case"chrome":
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        EventFiringWebDriver wrappedDriver= new EventFiringWebDriver(driver);
-        wrappedDriver.register(new ua.EventHandler());
-        return wrappedDriver;
-    }
-    public static void quitDriver(WebDriver driver) {driver.quit();
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/drivers/chromedriver.exe");
+                return new ChromeDriver();
 
-    }
+            case"ie":
+                    System.setProperty("webdriver.ie.driver",  System.getProperty("user.dir")+ "/drivers/IEDriverServer.exe");
+                return new InternetExplorerDriver();
+            default:System.setProperty("webdriver.chrome.driver", new File(DriverScript.class.getResource( "/chromedriver.exe").getFile()).getPath());
+                return new ChromeDriver();
+    }}
+        @BeforeClass
+        @Parameters("selenium.browser")
+    public void  getConfiguredDriver(@Optional("chrome") String browser){
+        WebDriver driver1=getDriver(browser);
+        driver1.manage().window().maximize();
 
+        driver1.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver= new EventFiringWebDriver(driver1);
+        driver.register(new ua.EventHandler());
+wait=new WebDriverWait(driver,5);
+    }
+    @AfterClass
+    public void quitDriver() {
+        if(driver!=null)
+            driver.quit();
+    }
 }
+
+
+
--- src/test/java/ua/Homework4.java
+++ src/test/java/ua/Homework4.java
@@ -2,18 +2,13 @@ package ua;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.nio.file.Paths;
 import java.util.UUID;
 
 public class Homework4 extends DriverScript {
@@ -38,7 +33,7 @@ public class Homework4 extends DriverScript {
     }
     @Test(dataProvider = "getLoginData")
     public void open(String login,String password){
-        EventFiringWebDriver driver=getConfiguredDriver();
+
         driver.navigate().to("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");//driver.get(Properties.getBaseAdminDir());}
         driver.findElement(eInput).sendKeys(login);
         driver.findElement(pInput).sendKeys(password);
@@ -68,76 +63,3 @@ public class Homework4 extends DriverScript {
         driver.findElement(switchInput).click();
         driver.findElement(submitProdact).click();
     }}
-/*package ua;
-
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
-import org.testng.annotations.*;
-
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-public class Homework4 extends DriverScript {
-
-    @Parameters("browser")
-    @BeforeMethod
-    public void getConfiguredDriver(@Optional("chrome") String browser){
-        driver=getDriver(browser);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);}
-    @DataProvider
-    public Object[][] getLoginData(){
-        return new String[][]{{"webinar.test@gmail.com","Xcg7299bnSmMuRLp9ITw"}};
-    }
-    private String name= UUID.randomUUID().toString();
-    private int count=(0 + (int) (Math.random() * 100));
-    private float price=(0 + (float) (Math.random() * 100));
-    private By category= By.id("subtab-AdminCatalog");
-    private By newCategory= By.id("page-header-desc-configuration-add");
-    private By nameCategory= By.id("form_step1_name_1");//("name_1");
-    private By eInput= By.id("email");
-    private By pInput= By.id("passwd");
-    private By loginBtn= By.name("submitLogin");
-    private By countI= By.cssSelector("#tab_step3 > a");
-    private By countIrandom= By.cssSelector("#form_step3_qty_0");
-    private By priceI= By.cssSelector("#tab_step2 > a");
-    private By priceIrandom= By.cssSelector("#form_step2_price");
-    private By switchInput= By.className("switch-input");
-    private By submitProdact= By.cssSelector("#form > div.product-footer > div.col-lg-6.text-lg-right > div > button.btn.btn-primary.js-btn-save");
-
-    @Test(dataProvider = "getLoginData")
-    public void open(String login,String password){
-        driver.navigate().to("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");//driver.get(Properties.getBaseAdminDir());}
-   driver.findElement(eInput).sendKeys(login);
-    driver.findElement(pInput).sendKeys(password);
-   driver.findElement(loginBtn).click();
-         wait.until(ExpectedConditions.elementToBeClickable(category));
-        WebElement element=driver.findElement(category);
-        Actions actions=new Actions(driver);
-        actions.moveToElement(element).build().perform();
-        wait.until(ExpectedConditions.elementToBeClickable( element.findElements(By.cssSelector("li")).get(0)));
-        element.findElements(By.cssSelector("li")).get(0).click();
-        System.out.println("Page title is: " +driver.getTitle());
-        wait.until(ExpectedConditions.elementToBeClickable( element.findElements(By.cssSelector("li")).get(0)));
-        element.findElements(By.cssSelector("li")).get(0).click();
-        System.out.println("Page title is: " +driver.getTitle());
-        Assert.assertEquals(driver.getTitle(),"товары • prestashop-automation", "Unexpected title");
-
-        wait.until(ExpectedConditions.elementToBeClickable(newCategory));
-        driver.findElement(newCategory).click();
-        wait.until(ExpectedConditions.elementToBeClickable(nameCategory));
-        driver.findElement(nameCategory).sendKeys(name);
-        driver.findElement(countI).click();
-        driver.findElement(countIrandom).sendKeys((count+""));
-        driver.findElement(priceI).click();
-        driver.findElement(priceIrandom).sendKeys((price+""));
-        driver.findElement(switchInput).click();
-
-        driver.findElement(submitProdact).click();
-  }}*/
-
-
